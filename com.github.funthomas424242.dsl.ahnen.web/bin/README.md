f1fe389a1dc58ab19376d9c7916013d42051e3c8
FunThomas424242 <funthomas424242@gmail.com>
2016-12-25 17:46:30 +0100

    deployment skipped for obsolete artifacts
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ com.github.funthomas424242.dsl.ahnen.web/bin/README.md
@@ -0,0 +1,21 @@
+# Editor für *.ahnen 
+
+
+# Aufrufmöglichkeiten
+
+ * mvn jetty:run -> startet die Webanwendung auf http://localhost:8080
+ * mvn jasmine:bdd -P jasmine-spec-runner -> startet eine Testserverinstance auf http://localhost:8234/
+ * 
+  
+
+
+## Quellen
+
+
+### AngularJS mit Maven
+
+ * https://blog.akquinet.de/2011/02/11/mavenizing-javascript-projects/
+ * https://blog-de.akquinet.de/2013/03/21/wartbare-rich-web-applications-mit-angularjs-teil-2/
+ * https://dzone.com/articles/java-maven-angularjs-seed
+ * https://ajsblackbelt.wordpress.com/2014/06/30/angularjs-integration-with-maven/
+ 
---
+++ com.github.funthomas424242.dsl.ahnen.web/bin/WebRoot/xtext-resources/generated/mode-ahnen.js
@@ -0,0 +1,35 @@
+define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(oop, mText, mTextHighlightRules) {
+	var HighlightRules = function() {
+		var keywords = "Adoptiert|Familie|Familienbuch|Keine|Kind|Leiblich|Letzte|Mutter|Organisation|Person|Sonstige|Sponsored|Stiefkind|Unbekannt|Unverheiratet|Vater|abgebrochen|als|andauernd|bei|bekannt|bis|import|in|keine|lebend|lebensgemeinschaft|ohne|unabsehbar|unbekannt|unerfasst|vermisst|von|weiblich";
+		this.$rules = {
+			"start": [
+				{token: "comment", regex: "\\/\\/.*$"},
+				{token: "comment", regex: "\\/\\*", next : "comment"},
+				{token: "string", regex: '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]'},
+				{token: "string", regex: "['](?:(?:\\\\.)|(?:[^'\\\\]))*?[']"},
+				{token: "constant.numeric", regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"},
+				{token: "lparen", regex: "[{]"},
+				{token: "rparen", regex: "[}]"},
+				{token: "keyword", regex: "\\b(?:" + keywords + ")\\b"}
+			],
+			"comment": [
+				{token: "comment", regex: ".*?\\*\\/", next : "start"},
+				{token: "comment", regex: ".+"}
+			]
+		};
+	};
+	oop.inherits(HighlightRules, mTextHighlightRules.TextHighlightRules);
+	
+	var Mode = function() {
+		this.HighlightRules = HighlightRules;
+	};
+	oop.inherits(Mode, mText.Mode);
+	Mode.prototype.$id = "xtext/ahnen";
+	Mode.prototype.getCompletions = function(state, session, pos, prefix) {
+		return [];
+	}
+	
+	return {
+		Mode: Mode
+	};
+});
